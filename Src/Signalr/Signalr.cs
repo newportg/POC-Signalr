3a186ffaa14ccc23f5c976e13c07029cdc3fb4b5
Gary Newport <Gary.Newport@knightfrank.com>
2023-04-14 15:20:39 +0100

    update

--- Src/Signalr/Signalr.cs
+++ Src/Signalr/Signalr.cs
@@ -27,5 +27,25 @@ namespace Signalr
             _logger.LogInformation($"SignalR Connection URL = '{connectionInfo.Url}'");
             return connectionInfo;
         }
+
+        [Function("BroadcastToAll")]
+        [OpenApiOperation(operationId: "BroadcastToAll")]
+        [OpenApiRequestBody(contentType: "text/plain", bodyType: typeof(string), Description = "message", Example = typeof(string))]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(SignalRMessageAction), Description = "The OK response")]
+        [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Description = "Configuration / Database issue")]
+        [SignalROutput(HubName = "HubValue", ConnectionStringSetting = "AzureSignalRConnectionString")]
+        public SignalRMessageAction BroadcastToAll([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            using var bodyReader = new StreamReader(req.Body);
+
+            _logger.LogInformation($"SignalR BroadcastToAlL");
+
+            return new SignalRMessageAction("newEvent")
+            {
+                // broadcast to all the connected clients without specifying any connection, user or group.
+                Arguments = new[] { bodyReader.ReadToEnd() },
+            };
+
+        }
     }
 }
